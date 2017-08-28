ec25949529c7aae1c3ca5c7aa3b82523ec1c8a65
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-28 15:30:12 +0200

    First commit, at part 3 enabling javascript

---
+++ spec/bmi_calculator_spec.js
@@ -0,0 +1,17 @@
+// spec/bmi_calculator.js
+
+describe("BMICalculator", funtion() {
+  var calculator;
+  var person;
+
+beforeEach(function() {
+  person = new Person({weight: 90, height: 186});
+  calculator = new BMICalculator();
+  });
+
+  it("calculates BMI for a person using metric method", function() {
+    calculator.metric_bmi(person);
+    expect(person.bmiValue).toEqual(26.01);
+  });
+  
+});
---
+++ src/person.js
@@ -0,0 +1,11 @@
+// src/person.js
+
+function Person(attr) {
+  this.weight = attr.weight;
+  this.height = attr.height;
+
+  Person.prototype.calculate_bmi = function() {
+    calculator = new BMICalculator();
+    calculator.metric_bmi(this);
+  };
+};
