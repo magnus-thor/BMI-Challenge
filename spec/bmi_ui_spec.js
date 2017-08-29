a70d37cb7a58baf8d2fc49b7fcace5f5fbca51a7
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-29 17:56:58 +0200

    specs for bmi_ui

--- spec/bmi_ui_spec.js
+++ spec/bmi_ui_spec.js
@@ -1,3 +1,4 @@
+
 // spec/bmi_ui_spec.js
 
 describe('BMI_UI - index.html', function() {
@@ -8,6 +9,11 @@ describe('BMI_UI - index.html', function() {
     $('#weight').val('90');
     $('#height').val('186');
     $('#calculate_metric').trigger('click');
+    $('#weight_st').val('14');
+    $('#weight_lb').val('2');
+    $('#height_f').val('6');
+    $('#height_i').val('1');
+    $('#calculate_imp').trigger('click');
   });
 
   it("displays BMI Value", function() {
@@ -17,4 +23,13 @@ describe('BMI_UI - index.html', function() {
   it("displays BMI Message", function() {
       expect($('#display_message').text()).toBe('and you are Overweight');
   });
+
+  it("displays BMI Value from Imperial calculations", function() {
+      expect($('#display_value_2').text()).toBe('Your BMI is 26.12');
+  });
+
+  it("displays BMI Message from Imperial calculations", function() {
+    expect($('#display_message_2').text()).toBe('and you are Overweight');
+  });
+
 });
--- src/bmi_ui.js
+++ src/bmi_ui.js
@@ -15,8 +15,8 @@ $(document).ready(function () {
     var h_i = parseFloat($('#height_i').val());
     var person_imp= new Person({weight_st: w_s, weight_lb: w_l, height_f: h_f, height_i: h_i});
     person_imp.calculate_bmi_imp();
-    $('#display_value').html('Your BMI is ' + person_imp.bmiValue);
-    $('#display_message').html('Your BMI is ' + person_imp.bmiMessage);
+    $('#display_value_2').html('Your BMI is ' + person_imp.bmiValue);
+    $('#display_message_2').html('and you are ' + person_imp.bmiMessage);
   })
 
 });
